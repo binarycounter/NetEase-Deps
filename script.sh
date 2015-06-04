4cc374117f6b388d6c20b5f61653338b8ce12208
root <root@bananapi.(none)>
2015-06-04 17:04:17 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,12 @@
+echo Decrypting secrets (Signing key, g-drive token)
+openssl aes-256-cbc -K $encrypted_d7f5e54ff428_key -iv $encrypted_d7f5e54ff428_iv -in secrets.tar.gz.enc -out secrets.tar.gz -d
+tar xvf secrets.tar.gz
+echo Building APK:
+echo Executing java -jar apktool.jar b ../NetEase-Translation -o ../NetEase.apk
+java -jar apktool.jar b ../NetEase-Translation -o ../NetEase.apk
+echo ------------------------------------
+echo Signing APK:
+echo Executing java -jar signapk.jar certificate.pem key.pk8 ../NetEase.apk NetEase_signed.apk
+java -jar signapk.jar secrets/certificate.pem secrets/key.pk8 ../NetEase.apk NetEase_signed.apk
+
+
